1c743e220880edeb73a742ca1cc485acf0afab32
Giles Dring <giles@dringtech.com>
2023-07-19 10:28:11 +0100

    Adding more sites to tech dashboard

--- _config.ts
+++ _config.ts
@@ -1,8 +1,10 @@
 import lume from "lume/mod.ts";
+
 import base_path from "lume/plugins/base_path.ts";
 import date from "lume/plugins/date.ts";
 import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
+import vento from "lume/plugins/vento.ts";
 
 const site = lume({
   src: './src',
@@ -17,6 +19,7 @@ site.use(metas({
 }));
 site.use(date());
 site.use(postcss({}));
+site.use(vento());
 
 site.copy('CNAME');
 site.copy('.nojekyll');
